3700e8244e9fa421dfc1e77121b06697a04fc874
Darryl Eychner <darryl.eychner@icloud.com>
2026-07-08 08:14:17 -0400

    CA1862: Use the 'StringComparison' method overloads to perform case-insensitive string comparisons

--- backend/FinTrak.Api/Controllers/PlaidController.cs
+++ backend/FinTrak.Api/Controllers/PlaidController.cs
@@ -223,10 +223,10 @@ public async Task<IActionResult> Sync([FromServices] PlaidClient plaid)
 
                 accounts.TryGetValue(t.AccountId!, out var account);
 
-                var categoryName = t.Name?.ToLower().Contains("deposit") == true
+                var categoryName = t.Name?.ToLower().Contains("deposit", StringComparison.CurrentCultureIgnoreCase) == true
                     ? "INCOME"
                     : t.PersonalFinanceCategory?.Primary ?? string.Empty;
-                var detailedCategoryName = t.Name?.ToLower().Contains("deposit") == true
+                var detailedCategoryName = t.Name?.ToLower().Contains("deposit", StringComparison.CurrentCultureIgnoreCase) == true
                     ? string.Empty
                     : t.PersonalFinanceCategory?.Detailed ?? string.Empty;
 
