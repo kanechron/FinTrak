abc186c420465d9c8ae780dd8b0a1c229218519d
James Dow <dowjames0903@gmail.com>
2026-07-10 14:20:50 -0400

    Added webhook support for autosyncing transactions when available.

---
+++ backend/FinTrak.Infrastructure/Migrations/20260710150648_LatestChanges.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinTrak.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class LatestChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsPending",
+                table: "Transactions",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "boolean",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsPending",
+                table: "Transactions",
+                type: "boolean",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "boolean");
+        }
+    }
+}
