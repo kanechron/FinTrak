82d96d97c27e4e8f7f600f2f23e99167f4cef33e
James Dow <dowjames0903@gmail.com>
2026-08-22 15:29:59 -0400

    Moved lingering query logic from ReportsController to ReportsRepository

--- backend/FinTrak.Core/Interfaces/IReportsRepository.cs
+++ backend/FinTrak.Core/Interfaces/IReportsRepository.cs
@@ -6,5 +6,11 @@ public interface IReportsRepository
 {
     Task<Dictionary<Guid, List<MonthlyDataPoint>>> GetDataPoints(Guid userId);
     Task<Dictionary<Guid, MonthlyDataPoint>> GetRunningTransactionTotal(Guid userId);
+    Task<List<CategorySpendingDto>> GetCategorySpending(Guid userId, DateOnly from, DateOnly to, Guid[]? categoryIds);
+    Task<List<CategoryDetailSpendingDto>> GetCategoryDetailSpending(Guid userId, Guid categoryId, DateOnly from, DateOnly to);
+    Task<List<MonthlySpendingDto>> GetMonthlySpending(Guid userId, DateOnly from, DateOnly to);
+    Task<List<TransactionDto>> GetMonthlyTransactions(Guid userId, DateOnly from, DateOnly to);
+    Task<List<CashFlowDto>> GetCashFlow(Guid userId, DateOnly from, DateOnly to);
+    Task<List<TransactionDto>> GetCashFlowTransactions(Guid userId, DateOnly from, DateOnly to);
 }
     public readonly record struct MonthlyDataPoint(decimal MonthlySum, DateOnly Month);
