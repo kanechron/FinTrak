6ed86a4024bea97748c20ee1e2c61f0032326bf3
James Dow <dowjames0903@gmail.com>
2026-08-22 17:02:57 -0400

    Run Prettier formatting on SAD feature files
    Missed before the original merge.

--- frontend/web/src/pages/Reports/Reports.tsx
+++ frontend/web/src/pages/Reports/Reports.tsx
@@ -1,8 +1,4 @@
-import { 
-  useState, 
-  useEffect, 
-  useMemo 
-} from 'react'
+import { useState, useEffect, useMemo } from 'react'
 import {
   getCategorySpending,
   getMonthlySpending,
@@ -13,12 +9,8 @@ import {
   getMonthlyTransactions,
   getLTERegression,
   type LTEForecastingResponse,
-
 } from '../../api/reports'
-import { 
-  getBudgets, 
-  type Budget 
-} from '../../api/budgets'
+import { getBudgets, type Budget } from '../../api/budgets'
 import {
   getTransactionsByCategory,
   getTransactionsByDetailedCategory,
@@ -31,9 +23,7 @@ import MonthlySpendingChart, {
 import BudgetPerformanceChart from '../../components/charts/BudgetPerformanceChart'
 import CashFlowChart from '../../components/charts/CashFlowChart'
 import CategoryDetailCarousel from '../../components/charts/CategoryDetailCarousel'
-import { 
-  isoDate 
-} from '../../utils/formatDate'
+import { isoDate } from '../../utils/formatDate'
 import ReportMenu from './ReportMenu'
 import FilterPanel from './FilterPanel'
 import TransactionPanel from './TransactionPanel'
